b021b3bc92e81101f40e066167cf83627739f868
Srivatsanray <86644183+Srivatsanray@users.noreply.github.com>
2024-01-02 01:46:53 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,10 +3,10 @@ import streamlit as st
 
 
 @st.cache_data(ttl=3600)
-def build_model(data, model_choice):
+def build_model(data, model_choice, cross_valid):
     setup(data=data, target="Target")
     st.dataframe(pull(), hide_index=True, use_container_width=True)
-    mod = create_model(model_choice,return_train_score=True)
+    mod = create_model(model_choice,cross_validation=cross_valid)
     evaluate_model(mod)
     st.dataframe(pull(), use_container_width=True)
     plot_model(mod, plot='confusion_matrix', display_format='streamlit')
